20c6db957b1f81894a05cfb3238e2038abeed6e0
Roderick <Roderickj800@gmail.com>
2023-11-29 18:18:10 -0500

    5.4.2

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -37,7 +37,13 @@ const validateSignup = [
 router.post("/", validateSignup, async (req, res) => {
   const { email, password, username, firstName, lastName } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
-  const user = await User.create({ email, username,firstName, lastName hashedPassword });
+  const user = await User.create({
+    email,
+    username,
+    firstName,
+    lastName,
+    hashedPassword,
+  });
 
   const safeUser = {
     id: user.id,
