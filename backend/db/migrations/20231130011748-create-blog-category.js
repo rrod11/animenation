81d8167351be3c0f693480a8769b9fb8c9b1b15f
Roderick <Roderickj800@gmail.com>
2023-11-29 20:20:53 -0500

    right before the joins table seeders

---
+++ backend/db/migrations/20231130011748-create-blog-category.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("BlogCategories", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      blog_id: {
+        type: Sequelize.INTEGER,
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "BlogCategories";
+    await queryInterface.dropTable(options);
+  },
+};
---
+++ backend/db/models/blogcategory.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model, Validator } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class BlogCategory extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  BlogCategory.init(
+    {
+      blog_id: {
+        type: DataTypes.INTEGER,
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: "BlogCategory",
+      defaultScope: {
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      },
+    }
+  );
+  return BlogCategory;
+};
