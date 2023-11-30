d9cf35d77521d7f3bfd1233104e81e2596e3258b
Roderick <Roderickj800@gmail.com>
2023-11-29 20:56:15 -0500

    render build 4

--- backend/db/seeders/20231130010914-demo-review.js
+++ backend/db/seeders/20231130010914-demo-review.js
@@ -11,26 +11,26 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   async up(queryInterface, Sequelize) {
     await Review.bulkCreate([
-      {
-        review:
-          "Literally the best anime to ever exist stop gawking and go give it a watch",
-        user_id: "1",
-        post_id: "1",
-        rating: "5",
-      },
-      {
-        review:
-          "An amazing watch you will not regret taking the time out your day",
-        user_id: "3",
-        post_id: "2",
-        rating: "5",
-      },
-      {
-        review: "I was hooked from the jump cant wait for the next season",
-        user_id: "2",
-        post_id: "3",
-        rating: "5",
-      },
+      // {
+      //   review:
+      //     "Literally the best anime to ever exist stop gawking and go give it a watch",
+      //   user_id: "1",
+      //   post_id: "1",
+      //   rating: "5",
+      // },
+      // {
+      //   review:
+      //     "An amazing watch you will not regret taking the time out your day",
+      //   user_id: "3",
+      //   post_id: "2",
+      //   rating: "5",
+      // },
+      // {
+      //   review: "I was hooked from the jump cant wait for the next season",
+      //   user_id: "2",
+      //   post_id: "3",
+      //   rating: "5",
+      // },
     ]);
   },
 
