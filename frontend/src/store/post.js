09b6d3620042d2916c3c84545eef5d342b8de8b2
Roderick <Roderickj800@gmail.com>
2023-12-03 17:06:25 -0500

    hop the post reducer is done

--- frontend/src/store/post.js
+++ frontend/src/store/post.js
@@ -13,7 +13,7 @@ const allPosts = (payload) => {
     payload,
   };
 };
-const currentUserPost = (payload) => {
+const currentUserPosts = (payload) => {
   return {
     type: CURRENT_USER_POST,
     payload,
@@ -43,3 +43,151 @@ const deletePost = (payload, postId) => {
     payload: postId,
   };
 };
+
+export const allThePosts = () => async (dispatch) => {
+  const response = await csrfFetch("/api/posts");
+  const posts = await response.json();
+  dispatch(allPosts(posts));
+  return posts;
+};
+
+export const getCurrentUserPosts = (user) => async (dispatch) => {
+  const response = await csrfFetch("/api/posts/current");
+  const posts = await response.json(user);
+  dispatch(currentUserPosts(posts));
+  return posts;
+};
+export const onePost = (postId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/spots/${postId}`);
+  const post = await response.json();
+  dispatch(getPost(post));
+  return post;
+};
+export const createAPost = (payload, images) => async (dispatch) => {
+  const response = await csrfFetch("/api/posts", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ ...payload }),
+  });
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(createPost(data));
+    // if (images.length === 1) {
+    //   await csrfFetch(`/api/posts/${data.id}/images`, {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({
+    //       url: images[0],
+    //       preview: false,
+    //     }),
+    //   });
+    //   return data;
+    // }
+    // if (images.length > 1) {
+    //   images.map(async (img) => {
+    //     await csrfFetch(`/api/posts/${data.id}/images`, {
+    //       method: "POST",
+    //       headers: { "Content-Type": "application/json" },
+    //       body: JSON.stringify({
+    //         url: img,
+    //         preview: false,
+    //       }),
+    //     });
+    //   });
+    return data;
+    // }
+  } else {
+    const errors = await response.json();
+    return errors;
+  }
+};
+
+export const updateAPost = (payload, images, postId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/posts/${postId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ ...payload }),
+  });
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(updatePost(data));
+    // if (images.length === 1) {
+    //   await csrfFetch(`/api/posts/${data.id}/images`, {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({
+    //       url: images[0],
+    //       preview: false,
+    //     }),
+    //   });
+    //   return data;
+    // }
+    // if (images.length > 1) {
+    //   images.map(async (img) => {
+    //     await csrfFetch(`/api/posts/${data.id}/images`, {
+    //       method: "POST",
+    //       headers: { "Content-Type": "application/json" },
+    //       body: JSON.stringify({
+    //         url: img,
+    //         preview: false,
+    //       }),
+    //     });
+    //   });
+    return data;
+    // }
+  } else {
+    const errors = await response.json();
+    return errors;
+  }
+};
+export const deleteAPost = (postId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/spots/${postId}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(),
+  });
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(deletePost(data, postId));
+  } else {
+    const errors = await response.json();
+    return errors;
+  }
+};
+
+const initialState = {};
+const postReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case ALL_POSTS: {
+      const postData = {};
+      action.payload.Posts.forEach((post) => {
+        postData[post.id] = post;
+      });
+      return postData;
+    }
+    case GET_POST:
+      newState = {};
+      newState[action.post.id] = action.post;
+      return newState;
+    case CREATE_POST:
+      newState = { ...state, [action.payload.id]: action.payload };
+      return newState;
+    case CURRENT_USER_POST:
+      newState = { ...action.payload };
+      return newState;
+    case DELETE_POST:
+      let deleteState;
+      console.log(
+        "🚀 ~ file: posts.js:186 ~ postReducer ~ action.payload:",
+        action.payload
+      );
+      deleteState = { ...state };
+      delete deleteState[action.payload];
+      return deleteState;
+    default:
+      return state;
+  }
+};
+
+export default postReducer;
